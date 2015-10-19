e45d44a9a9f2b10550b7c69c24655a4eb2186a66
Caleb Morris <caleb.morris.g@gmail.com>
2015-10-18 22:09:17 -0700

    Added isomorphic words problem

---
+++ IsomorphicWords/IsomorphicWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Test.IsomorphicWords
+{
+
+	using System.Collections.Generic;
+	public class IsomorphicWords
+	{
+		bool IsIsomorphic(string left, string right) {
+			Dictionary<char, char> map = new Dictionary<char, char> ();
+			Dictionary<char, char> rmap = new Dictionary<char, char> ();
+			for (int i = 0; i < left.Length; i++) {
+				if (!map.ContainsKey (left [i]) && !rmap.ContainsKey(right[i])) {
+					map [left [i]] = right [i];
+					rmap [right [i]] = left [i];
+				} else if (
+					!map.ContainsKey (left [i]) ||
+					!rmap.ContainsKey(right[i]) ||
+					map [left [i]] != right [i] ||
+					rmap[right[i]] != left[i]
+				) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public int countPairs(string[] words) {
+			int count = 0;
+			for (int i = 0; i < words.Length - 1; i++) {
+				for (int j = i + 1; j < words.Length; j++) {
+					if (IsIsomorphic (words [i], words [j])) {
+						count++;
+					}
+				}
+			}
+			return count;
+		}
+	}
+}
+
---
+++ IsomorphicWords/IsomorphicWordsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test.IsomorphicWords
+{
+	[TestFixture]
+	public class IsomorphicWordsTests
+	{
+		IsomorphicWords handler;
+		
+		[SetUp]
+		public void Setup() {
+			handler = new IsomorphicWords ();
+		}
+
+		[Test]
+		public void Test1() {
+			Assert.AreEqual (1, handler.countPairs (new string[] { "abca", "zbxz", "opqr" }));
+		}
+
+		[Test]
+		public void Test2() {
+			Assert.AreEqual (4, handler.countPairs (new string[] {"aa", "ab", "bb", "cc", "cd"}));
+		}
+	}
+}
+
