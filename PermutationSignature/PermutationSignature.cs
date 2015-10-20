3f7f44f1f938e22e43104b3b0d739b78f7de0a7a
Caleb Morris <caleb.morris.g@gmail.com>
2015-10-19 22:17:18 -0700

    Added SRM 397 PermutationSignature

---
+++ PermutationSignature/PermutationSignature.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Test.PermutationSignature
+{
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public struct Permutation {
+		public List<int> currentList;
+		public string remainingSignature;
+		public int highest;
+		public Permutation(List<int> c, string r, int h) {
+			this.currentList = c;
+			this.remainingSignature = r;
+			this.highest = h;
+		}
+
+		public static Permutation InitialPermutation(string signature) {
+			return new Permutation (new List<int> { 1 }, signature, 1);
+		}
+	}
+
+	public class PermutationSignature
+	{
+		private Permutation TransformSignature(Permutation currentPermutation) {
+			if (currentPermutation.remainingSignature.Length == 0) {
+				return currentPermutation;
+			}
+			if (currentPermutation.remainingSignature [0] == 'I') {
+				currentPermutation.currentList.Add (currentPermutation.highest + 1);
+				return TransformSignature (new Permutation(
+					currentPermutation.currentList,
+					currentPermutation.remainingSignature.Substring(1),
+					currentPermutation.highest + 1
+				));
+			} else { // 'D'
+				int i;
+				for (i = currentPermutation.currentList.Count - 1; i >= 1; i--) {
+					if (currentPermutation.currentList [i - 1] > currentPermutation.currentList [i]) {
+						currentPermutation.currentList [i] += 1;
+					} else {
+						break;
+					}
+				}
+
+				currentPermutation.currentList [i] += 1;
+
+				currentPermutation.currentList.Add (currentPermutation.currentList.Last() - 1);
+				return TransformSignature (new Permutation(
+					currentPermutation.currentList,
+					currentPermutation.remainingSignature.Substring(1),
+					Math.Max(currentPermutation.highest, currentPermutation.currentList[i])
+				));
+			}
+		}
+
+		public int[] reconstruct(string signature) {
+			return TransformSignature (Permutation.InitialPermutation(signature)).currentList.ToArray();
+		}
+	}
+}
+
---
+++ PermutationSignature/PermutationSignatureTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test.PermutationSignature
+{
+	[TestFixture]
+	public class PermutationSignatureTests
+	{
+		private PermutationSignature m_ps;
+
+		[SetUp]
+		public void Setup ()
+		{
+			m_ps = new PermutationSignature ();
+		}
+
+		[Test]
+		public void SimpleTests() {
+			Assert.AreEqual (new int[] { 2, 1 }, m_ps.reconstruct ("D"));
+			Assert.AreEqual (new int[] { 1, 2 }, m_ps.reconstruct ("I"));
+			Assert.AreEqual (new int[] { 4, 3, 2, 1 }, m_ps.reconstruct ("DDD"));
+			Assert.AreEqual (new int[] { 1, 2, 3, 4 }, m_ps.reconstruct ("III"));
+		}
+
+		[Test]
+		public void HarderTests() {
+			Assert.AreEqual (new int[] { 2, 1, 4, 3 }, m_ps.reconstruct ("DID"));
+			Assert.AreEqual (new int[] { 1, 3, 2, 4 }, m_ps.reconstruct ("IDI"));
+		}
+
+		[Test]
+		public void GivenTests() {
+			Assert.AreEqual (new int[] { 1, 2, 3, 4, 5, 6 }, m_ps.reconstruct ("IIIII"));
+			Assert.AreEqual (new int[] { 2, 1, 3 }, m_ps.reconstruct ("DI"));
+			Assert.AreEqual (new int[] { 1, 2, 3, 4, 6, 5 }, m_ps.reconstruct ("IIIID"));
+			Assert.AreEqual (new int[] { 2, 1, 3, 5, 4, 7, 6 }, m_ps.reconstruct ("DIIDID"));
+		}
+	}
+}
+
